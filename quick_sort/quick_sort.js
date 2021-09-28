d1234d0bd6d2c6db519fb04ad18f092b79e8b3c5
clark-cui <cuirongchuan@163.com>
2021-09-28 16:48:07 +0800

    fix md

---
+++ quick_sort/quick_sort.js
@@ -0,0 +1,16 @@
+const quick_sort = (q, l, r) => {
+  if (l >= r) return;
+  let i = l - 1,
+    j = r + 1,
+    x = q[l + r >> 1];
+  while (i < j) {
+    do i++; while (q[i] < x);
+    do j--; while (q[j] > x);
+    if (i < j) {
+      let mid = q[i];
+      q[i] = q[j];
+      q[j] = mid;
+    }
+  }
+  quick_sort(q, l, j), quick_sort(q, j + 1, r);
+}
