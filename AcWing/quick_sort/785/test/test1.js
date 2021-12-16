295cc56a035485cbcad339c0975e03a34d4fc2a0
clark-cui <cuirongchuan@163.com>
2021-12-16 11:25:27 +0800

    chore: add test1

---
+++ AcWing/quick_sort/785/test/test1.js
@@ -0,0 +1,23 @@
+// test case
+const x = [3, 1, 2, 4, 5]
+
+const quick_sort = (q, l, r) => {
+  if (l >= r) return;
+  let i = l - 1,
+    j = r + 1,
+    x = q[l + r >> 1];
+  while (i < j) {
+    do i++; while (q[i] < x);
+    do j--; while (q[j] > x);
+    if (i < j) {
+      let mid = q[i];
+      q[i] = q[j];
+      q[j] = mid
+    }
+  }
+  quick_sort(q, l, j), quick_sort(q, j + 1, r)
+
+}
+
+quick_sort(x, 0, x.length - 1)
+console.log(x)
