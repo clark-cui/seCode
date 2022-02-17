33c5151fca28cad86fa72157a9ab011a34746392
clark-cui <cuirongchuan@163.com>
2022-02-17 12:22:12 +0800

    chore: init instanceof

---
+++ test/instanceof.js
@@ -0,0 +1,13 @@
+
+const myInstanceof=(target,origin)=>{
+
+  while(target){
+    if(target.__proto__=origin.prototype){
+      return true;
+    }
+    target=target.__proto__;
+  }
+  return false;
+}
+
+// react  
