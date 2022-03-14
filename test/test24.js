89e2a4792fea118a4855e32e01c126d6d15001b4
clark-cui <cuirongchuan@163.com>
2022-03-15 00:34:12 +0800

    chore: lint

--- test/test24.js
+++ test/test24.js
@@ -24,26 +24,27 @@ console.log(getDifferentChild(arr));
 
 
 
-// function reverse(q){
-//   const result=[];
-//   for(let i=0;i<q.length;i++){
-//     result.unshift(q[i])
-//   }
-//   return result;
-// }
-// console.log(reverse(arr));
+function reverse(q) {
+  const result = [];
+  for (let i = 0; i < q.length; i++) {
+    result.unshift(q[i])
+  }
+  return result;
+}
+console.log(reverse(arr));
 
-// function betterReverse(q){
-//   let mid =Math.floor((q.length-1)/2);
-//   console.log(mid);
-//   let i=mid-1,j=mid+1;
-//   while(i>=0&&j<=q.length-1){
-//     let stage=q[i];
-//     q[i]=q[j];
-//     q[j]=stage;
-//     i--;
-//     j++;
-//   }
-//   return q;
-// }
+function betterReverse(q) {
+  let mid = Math.floor((q.length - 1) / 2);
+  console.log(mid);
+  let i = mid - 1,
+    j = mid + 1;
+  while (i >= 0 && j <= q.length - 1) {
+    let stage = q[i];
+    q[i] = q[j];
+    q[j] = stage;
+    i--;
+    j++;
+  }
+  return q;
+}
 // console.log(betterReverse(arr));
